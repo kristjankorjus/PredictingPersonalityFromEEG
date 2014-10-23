dd838507ba600ab77527dcd2d544eb036958de46
Kristjan Korjus <korjus@gmail.com>
2014-10-23 12:59:12 +0300

    Change of the pipeline for revision 1
    Changed almost everything :)

---
+++ Scripts, functions, data and results/NestedCV.m
@@ -0,0 +1,72 @@
+
+% This script performs a nested cross-validation analysis for the article 
+% "Personality cannot be predicted from the power of resting state 1 EEG"
+% submitted to Frontiers in Human Neuroscience
+
+% First, it calls a function Step1_nested_cross_validation.m, which chooses
+% the best hyper-parameters
+
+% Second, it predicts personality scores for each personality trait using
+% the function Step2_cross_validation.m
+
+% Third, it performs statistical significance analysis
+
+% Tested with Windows 8 and Matlab 2013a with Statistics Toolbox
+% Comments and questions: Kristjan Korjus (korjus@gmail.com)
+
+%% Init
+
+% Load data
+load('Classes');
+load('DataOpen');
+load('DataClose');
+%load('Scores.mat');
+
+% Fixed parameters
+k_fold_parameter = 10; % k-fold parameter in cross-validation
+nested_k_fold_parameter = 10; % k-fold parameter in nested-cross-validation
+
+
+%% Cross-Validation partitions
+
+% Random Partitions for the first cross-validation
+partitions = crossvalind('Kfold', size(DataOpen,1), k_fold_parameter);
+
+% Will save number of correct predictions for each personality score
+Correct = zeros(1,5);
+
+for ii = 1:k_fold_parameter
+  
+  % Indeces for test and train
+  test_id = (partitions == ii);
+  train_id = ~test_id;
+    
+  % Nested cross-validation for choosing the best parameters
+  % Note: only train_id goes in
+  
+  BestParameters = Step1_nested_cross_validation(DataOpen(train_id,:,:), ...
+    DataOpen(train_id,:,:), Classes(train_id,:), nested_k_fold_parameter,ii);
+  
+  % Will use the best parameters to classify test_id subjects   
+  for jj = 1:5 % All five personality traits
+    
+    % Keeping only the parameters
+    Parameters = BestParameters(jj,7:14);
+
+    % Adding the number of correct predictions
+    Correct(jj) = Correct(jj) + Step2_cross_validation(DataOpen, ...
+    DataClose, Classes(:,jj), Parameters, train_id, test_id);
+  
+  end
+end
+
+%% Statistical significance - using binomial test
+
+% Number of samples
+n=309;
+
+% Errors
+Errors = (n - CorrectSum) / n;
+
+% P-values
+p_valules = binocdf(n-CorrectSum,n,0.5);
---
+++ Scripts, functions, data and results/Step1_nested_cross_validation.m
@@ -0,0 +1,396 @@
+function [ Parameters ] = Step1_nested_cross_validation( DataOpen, DataClose, ...
+  Classes, k_fold, partition_ID)
+% Step1_nested_cross_validation does a cross-validation with all the models
+
+% Function performs a nested cross-validation step for the article 
+% "Personality cannot be predicted from the power of resting state 1 EEG"
+% submitted to Frontiers in Human Neuroscience
+%
+% Output: the best hyper-parameters for each personality trait
+%
+% Tested with Windows 8 and Matlab 2013a with Statistics Toolbox
+%
+% Comments and questions: Kristjan Korjus (korjus@gmail.com)
+
+%% Finding best hyper parameters with k-fold cross-validation
+
+% Fixed parameters
+num_of_samples = size(DataOpen,1);
+
+%%%%%%%%%%%%%%%%%%%
+% Parameter space %
+%%%%%%%%%%%%%%%%%%%
+
+% Using different types of data:
+DataType = 1:2; % Eyes open, eyes close
+
+% Different Regions of Interests (ROI):
+Roi = 1:2; % All, Literature
+
+% Different frequency bands:
+Freq = 1:3; % All, customized pooling, based on literature
+
+% Normalization:
+Norm = 1:3; % Column normalization, Row normalization, No normalization
+
+% Choosing number of dimensions by total variance explained by PCA:
+Var = [0.7, 0.9, NaN]; % Variance explained by PCA or no PCA
+
+% C parameters for SVM
+C = [0.01, 100];
+
+% Choosing the model:
+% Model = 1:2; % Linear SVM, SVM with RBF kernel
+
+% Finding RBF sigma parameter for SVM:
+RBFerror = [0.1, 0.3]; % RBF sigma parameter error rate for non-linear SVM
+
+ParemeterSpace = length(DataType)*length(Roi)*length(Freq)*length(Norm)*...
+  length(Var)*length(C)*3;
+
+% Random Partitions
+partitions = crossvalind('Kfold', num_of_samples, k_fold);
+
+%% Master for-loop
+
+% Results
+Results=[];
+
+% Different data used
+for iDataType = DataType
+  
+  if iDataType == 1
+    Data = DataOpen;
+  elseif iDataType == 2
+    Data = DataClose;
+  else
+    Data = [DataOpen,DataClose];
+    clear('DataOpen');
+    clear('DataClose');
+  end
+  
+  % Size of the data
+  [num_of_samples, NumOfChannels, NumOfSpectrumPoints] = size(Data);
+  
+  % Different Regions of Interests
+  for iRoi = Roi
+
+    % If must take ROIs
+    if iRoi == 2
+      
+      %Temporary variable for single data
+      DataTemp = zeros(num_of_samples,7,NumOfSpectrumPoints);
+      
+      % lFr = 'F3', 'F7', 'AF3', 'Fp1'
+      DataTemp(:,1,:) = mean(Data(:,[4, 3, 2, 1],:),2);
+      % rFr = 'F4', 'F8', 'AF4', 'Fp2
+      DataTemp(:,2,:) = mean(Data(:,[27, 28, 29, 30],:),2);
+      % lCnt = 'FC5', 'CP5', 'T7', 'P7', 'C3'
+      DataTemp(:,3,:) = mean(Data(:,[6, 10, 7, 11, 8],:),2);
+      % rCnt = 'FC6', 'CP6', 'T8', 'P8', 'C4'
+      DataTemp(:,4,:) = mean(Data(:,[25, 21, 24, 20, 23],:),2);
+      % mFr = 'Fz', 'Cz', 'FC1', 'FC2'
+      DataTemp(:,5,:) = mean(Data(:,[31, 32, 5, 26],:),2);
+      % mPar = 'Pz', 'CP1', 'CP2', 'P3', 'P4'
+      DataTemp(:,6,:) = mean(Data(:,[13, 9, 22, 12, 19],:),2);
+      % Occ = 'PO3' 'PO4', 'O1', 'Oz', 'O2'
+      DataTemp(:,7,:) = mean(Data(:,[14, 18, 15, 16, 17],:),2);
+
+      Data1 = DataTemp;
+     
+      %Changing the number of channels variable
+      NumOfChannels = 7;
+      
+      % If using both data sets then second half needs ROI also 
+      if iDataType == 3
+        
+        %Temporary variable for data
+        DataTemp=zeros(num_of_samples,7,NumOfSpectrumPoints);
+    
+        % lFr = 'F3', 'F7', 'AF3', 'Fp1'
+        DataTemp(:,1,:) = mean(Data(:,32+[4, 3, 2, 1],:),2);
+        % rFr = 'F4', 'F8', 'AF4', 'Fp2
+        DataTemp(:,2,:) = mean(Data(:,32+[27, 28, 29, 30],:),2);
+        % lCnt = 'FC5', 'CP5', 'T7', 'P7', 'C3'
+        DataTemp(:,3,:) = mean(Data(:,32+[6, 10, 7, 11, 8],:),2);
+        % rCnt = 'FC6', 'CP6', 'T8', 'P8', 'C4'
+        DataTemp(:,4,:) = mean(Data(:,32+[25, 21, 24, 20, 23],:),2);
+        % mFr = 'Fz', 'Cz', 'FC1', 'FC2'
+        DataTemp(:,5,:) = mean(Data(:,32+[31, 32, 5, 26],:),2);
+        % mPar = 'Pz', 'CP1', 'CP2', 'P3', 'P4'
+        DataTemp(:,6,:) = mean(Data(:,32+[13, 9, 22, 12, 19],:),2);
+        % Occ = 'PO3' 'PO4', 'O1', 'Oz', 'O2'
+        DataTemp(:,7,:) = mean(Data(:,32+[14, 18, 15, 16, 17],:),2);
+
+        Data1 = [Data1, DataTemp];
+        
+        %Changing the number of channels variable
+        NumOfChannels = 14;
+      end
+    else
+      Data1=Data;
+    end
+    
+    clear('DataTemp');
+
+    % Different frequency bands (will transform 3D matrix to 2D)
+    for iFreq = Freq
+
+      % All frequencies
+      if iFreq == 1
+        
+        % Initialize
+        DataMatrix = zeros(num_of_samples, NumOfChannels*NumOfSpectrumPoints);
+        
+        % For all samples
+        for ii = 1:num_of_samples
+          count = 1;
+          % For each channel
+          for kk = 1:NumOfChannels;
+            DataMatrix(ii, count:count+NumOfSpectrumPoints-1) = squeeze(Data1(ii, kk,:));
+            count = count + NumOfSpectrumPoints;
+          end
+        end
+
+      % Bands based on power spectrum
+      elseif iFreq == 2
+
+        % Initialize
+        DataMatrix = zeros(num_of_samples,NumOfChannels*98);
+
+        for ii = 1:num_of_samples
+          count = 0;
+          for kk = 1:NumOfChannels;
+            for jj = 1:60
+              count = count + 1;
+              DataMatrix(ii,count) =  Data1(ii,kk,jj);
+            end
+            for jj = 61:2:99
+              count = count + 1;
+              DataMatrix(ii,count) = mean(Data1(ii,kk,jj:jj+1));
+            end
+            for jj = 101:4:145
+              count = count + 1;
+              DataMatrix(ii,count) = mean(Data1(ii,kk,jj:jj+3));
+            end
+            for jj = 149:40:349
+              count = count + 1;
+              DataMatrix(ii,count) = mean(Data1(ii,kk,jj:jj+39));
+            end
+          end
+        end
+
+      % Bands based on literature  
+      else
+
+        % Initialize
+        DataMatrix = zeros(num_of_samples,NumOfChannels*11);
+
+        for ii = 1:num_of_samples
+          for jj = 1:NumOfChannels
+            % "sub-delta"
+            DataMatrix(ii,(jj-1)*11+1)=mean(Data1(ii,jj,2:5));
+            % delta
+            DataMatrix(ii,(jj-1)*11+2)=mean(Data1(ii,jj,6:17));
+            % theta
+            DataMatrix(ii,(jj-1)*11+3)=mean(Data1(ii,jj,18:33));
+            % alpha
+            DataMatrix(ii,(jj-1)*11+4)=mean(Data1(ii,jj,34:49));
+            % low beta
+            DataMatrix(ii,(jj-1)*11+5)=mean(Data1(ii,jj,50:81));
+            % high beta
+            DataMatrix(ii,(jj-1)*11+6)=mean(Data1(ii,jj,82:121));
+            % lowlow gamma
+            DataMatrix(ii,(jj-1)*11+7)=mean(Data1(ii,jj,122:161));
+            % low gamma
+            DataMatrix(ii,(jj-1)*11+8)=mean(Data1(ii,jj,162:200));
+            % high gamma
+            DataMatrix(ii,(jj-1)*11+9)=mean(Data1(ii,jj,201:279));
+            % highhigh gamma
+            DataMatrix(ii,(jj-1)*11+10)=mean(Data1(ii,jj,280:359));
+            % superhigh gamma
+            DataMatrix(ii,(jj-1)*11+11)=mean(Data1(ii,jj,360:398));
+          end
+        end
+      end
+
+      % Normalization
+      for iNorm = Norm
+        
+        % If normalization needed then do it
+        if iNorm ~= 3
+          DataMatrix2 = zscore(DataMatrix,[],iNorm);
+        else
+          DataMatrix2 = DataMatrix;
+        end
+
+
+        % Performing PCA
+        [~, Score, Eigen] = princomp(DataMatrix2,'econ');
+        
+        % Calculating cumulative variance
+        CumEigen = cumsum(Eigen)/sum(Eigen);
+        
+        % Amount of variance explained by dimensionality reduction
+        for iVar = 1:length(Var)
+
+          if ~isnan(Var(iVar))
+            % Choosing the number of dimensions by cumulative variance
+            MaxId = find(CumEigen>Var(iVar),1);
+
+            % Data after the PCA transformation
+            DataMatrix3 = Score(:,1:MaxId);
+          else
+            DataMatrix3 = DataMatrix2;
+          end
+
+   
+          for iC = 1:length(C)
+            
+            % Catching errors if linear SVM does not converge
+            try
+            % Nested-Cross-Validation
+            
+              % Init
+              CorrectLinear = zeros(1,5);
+              
+              for i_partition = 1:k_fold
+
+                % Indeces for test and train
+                test_id = (partitions == i_partition);
+                train_id = ~test_id;
+
+                % Getting the data
+                xtrain = DataMatrix3(train_id,:);
+                ytrain = Classes(train_id,:);
+
+                xtest = DataMatrix3(test_id,:);
+                ytest = Classes(test_id,:);
+
+                % Different personality traits
+                for ii = 1:5
+
+                  % linear SVM, function handle
+                  classified_classes = svmclassify(...
+                    svmtrain(xtrain,ytrain(:,ii),'boxconstraint',C(iC),...
+                    'kktviolationlevel',0.01,...
+                    'options',optimset('maxiter',1000000)),xtest);
+
+                  % Adding if the result is correct
+                  CorrectLinear(ii) = CorrectLinear(ii) + ...
+                    sum(classified_classes == ytest(:,ii));
+                end
+              end
+                
+              ErrorLinear = (num_of_samples - CorrectLinear)/num_of_samples;
+              
+              % Saving results
+              Results(end+1,:) = [ErrorLinear, mean(ErrorLinear), ...
+                iDataType, iRoi, iFreq, iNorm, iVar, iC, 1, ...
+                NaN, NaN, MaxId];
+
+            catch
+
+              % Saving NaNs if linear model did not converge
+              Results(end+1,:) = [NaN, NaN, NaN, NaN, NaN, NaN, ...
+                iDataType, iRoi, iFreq, iNorm, iVar, iC, 1, ...
+                NaN, NaN, MaxId];
+              
+            end
+
+            % Error in RBF sigma estimation
+            for iRBFerror = 1:length(RBFerror)
+
+              % Finding RBF parameter for SVM with random classes
+
+              ClassesRandInit = zeros(num_of_samples,1);
+              ClassesRandInit(1:ceil(num_of_samples / 2)) = 1;
+              ClassesRand = ClassesRandInit(randperm(num_of_samples));
+              
+              % If does not converge then sigma will be 100 001
+              RBFsigma=100001;
+              
+              % Sigma values tried
+              Values = [0.1.^(5:-1:1), 0.1:0.1:1, 2:1:20, 25:5:100, ...
+                1000:1000:10000, 20000:10000:100000];
+              
+              % Try out increasing sigma values until error is reached
+              for jj = 1:length(Values)
+                
+                % Train and classify same points
+                RE = svmclassify(svmtrain(DataMatrix3,ClassesRand,...
+                  'kernel_function','rbf','rbf_sigma',Values(jj),...
+                  'boxconstraint',C(iC),'options',...
+                  optimset('maxiter',100000)),DataMatrix3);
+                
+                % Save the results
+                ErrorTemp = sum(abs(RE - ClassesRand)) / num_of_samples;
+                
+                % If needed error is reached, break and continue
+                if  ErrorTemp > RBFerror(iRBFerror)
+                  RBFsigma = Values(jj);
+                  break
+                end
+              end
+          
+              % Results for SVM with RBF kernel
+              CorrectRBF = zeros(1,5);
+                            
+              % Nested-Cross-Validation
+              for i_partition = 1:k_fold
+                
+                % Indeces for test and train
+                test_id = (partitions == i_partition);
+                train_id = ~test_id;
+                
+                % Getting the data
+                xtrain = DataMatrix3(train_id,:);
+                ytrain = Classes(train_id,:);
+
+                xtest = DataMatrix3(test_id,:);
+                ytest = Classes(test_id,:);
+
+                % Different personalities
+                for ii = 1:5
+                  % RBF kernel SVM
+                  classified_classes = svmclassify(svmtrain(...
+                    xtrain,ytrain(:,ii),'kernel_function','rbf',...
+                    'rbf_sigma',RBFsigma,'boxconstraint',C(iC),...
+                    'options',optimset('maxiter',100000)),xtest);
+                  
+                  % Adding if the result is correct
+                  CorrectRBF(ii) = CorrectRBF(ii) + ...
+                    sum(classified_classes == ytest(:,ii));
+                end
+              end
+              
+              ErrorRBF = (num_of_samples - CorrectRBF) / num_of_samples;
+              
+              % Saving results
+              Results(end+1,:) = [ErrorRBF, mean(ErrorRBF), ...
+                iDataType, iRoi, iFreq, iNorm, iVar, iC, 2, ...
+                iRBFerror, RBFsigma, MaxId];
+            end
+            
+            % Print out the progress
+            fprintf(1,'Inside partition %6.2f, nested CV: %6.2f %%\n',...
+              partition_ID, size(Results,1)/ParemeterSpace*100);
+            
+          end
+        end
+      end
+    end
+  end
+end
+
+% Choosing and saving only the best parameters
+Parameters = zeros(5,size(Results,2));
+for ii = 1:5
+  row = find(Results(:,ii) == min(Results(:,ii)),1);
+  Parameters(ii,:) = Results(row,:);
+end
+
+% Saving the results just in case
+save(strcat('Results',num2str(partition_ID),'.mat'),'Results');
+
+end
